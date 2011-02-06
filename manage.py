d8dcd0d6f53e99f6fa9f1980eeef733f05d2d90e
Brian Wilson <brianwilson1001@hotmail.com>
2011-02-06 21:12:12 +0000

    Added sqlalchemy_migrate database tracking Created initial user table version

---
+++ manage.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+from migrate.versioning.shell import main
+main(url='mysql://tracker:X6AbqNhiulEyMHo5F71L@79.125.121.165/timesheet', debug='False', repository='tracker-db-repo')
