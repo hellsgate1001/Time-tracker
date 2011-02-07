3144ac4624dfdad4e0fee002101a66151cf848b6
Brian Wilson <brianwilson1001@hotmail.com>
2011-02-07 07:19:49 +0000

    Initial attempt at starting to use sqlalchemy-migrate

---
+++ 002_Add_user-category_table.py
@@ -0,0 +1,20 @@
+from sqlalchemy import *
+from migrate import *
+
+meta = MetaData()
+
+user_category = Table('user_category', meta,
+    Column('id', Integer, primary_key=True),
+    Column('category', String(40))
+)
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind migrate_engine
+    # to your metadata
+    meta.bind = migrate_engine
+    user_category.create()
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    meta.bind = migrate_engine
+    #user_category.drop()
---
+++ tracker-db-repo/versions/002_Add_user_table.py
@@ -0,0 +1,31 @@
+from sqlalchemy import *
+from migrate import *
+from migrate.changeset import *
+
+meta = MetaData()
+user_category = Table('user_category', meta)
+
+user = Table('user', meta,
+    Column('id', Integer, primary_key=True),
+    Column('email', String(255)),
+    Column('first_name', String(40)),
+    Column('surname', String(40)),
+    Column('password', String(255)),
+    Column('user_category', Integer, ForeignKey("user_category.id")),
+    ForeignKeyConstraint(
+                    ['user_category'],
+                    ['user_category.id'],
+                    onupdate="CASCADE", ondelete="SET NULL"
+        )
+)
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind migrate_engine
+    # to your metadata
+    meta.bind = migrate_engine
+    user.create()
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    meta.bind = migrate_engine
+    user.drop()
